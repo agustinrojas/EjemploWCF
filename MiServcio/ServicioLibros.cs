c5eddb8d1ff750aebac67d8ff982ba06f9dbc28d
ATMIRA\agustin.rojas <agustin.rojas@atmira.com>
2018-05-03 20:32:24 +0200

    Proyecto Ejemplo WCF

---
+++ MiServcio/ServicioLibros.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MiServcio
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código y en el archivo de configuración a la vez.
+    public class ServicioLibros : IServicioLibros
+    {
+        static List<string> lista = new List<string>();
+        public void AddLibros(string nombre)
+        {
+            lista.Add(nombre);
+        }
+
+        public void BorrarLibros(string nombre)
+        {
+            lista.Remove(nombre);
+        }
+
+        public string[] ListaLibros()
+        {
+            return lista.ToArray();
+        }
+    }
+}
